75f9eeb1144f4f80a8123ecc88c5919389530794
Tanner <tannerchm@yahoo.com>
2018-07-24 22:07:29 -0500

    edit markdown

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ajaxHomework
-My first attempt at API alone
+# Giphy AJAX App
+
+I used the GIPHY API to make a dynamic web page that populates with gifs of my favorite movies. I call the GIPHY API and use JavaScript and jQuery to change the HTML of your site.
+
+
+
+1. **Hit the GIPHY API**.
+
+2. **The app takes the topics in this array and create buttons in HTML.**
+
+3. **When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.**
+
+4. **When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.**
+
+   
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=";
-// Array of favorite movies
+    // Array of favorite movies
     var topics = ["jurassic park", "back to the future", "jaws", "terminator", "harry potter", "universal monsters", "shrek", "despicable me", "men in black", "spiderman", "ghostbusters", "ET", "king kong", "twister"];
      
     // function to render buttons
@@ -16,51 +16,52 @@ $(document).ready(function() {
             a.attr("data-name", topics[i]);
             a.text(topics[i]);
             $("#buttons-view").prepend(a);
-       }};
-
-    //    function to create a button
-       function addNewButton(){
-        $("#buttons-view").on("click", function(){
-            var movie = $("#add-movie").val().trim();
-            topics.push(movie);
-            renderButtons();
-        });
-    }
-
-
+        }
+    };
+    renderButtons();    // JUST ADDED THIS HERE SO THE BUTTONS COME UP WHEN THE PAGE LOADS
 
     // prevents page from reloading on click
-    $("button").on("click", function(event) {
+    $("#add-movie").on("click", function(event) {
         event.preventDefault();
+        var newMovie = $("#movie-input").val().trim();
+        topics.push(newMovie);
+        renderButtons();
     });
 
-// Diaplay the gifs
-    function displayMovies(){
-        var movieInfo = $(this).attr("movie-name");
+
+// Display the gifs
+    $(document).on("click", ".movie", function displayMovies(){
+        var movieInfo = $(this).attr("data-name");
         // passes the movie information into the giphy api with a limit of 10 per page
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movieInfo + "&api_key=c3kGLRZNonrr45v8KXdkfPf7J48L86IA&limit=10";
-        console.log(queryURL);
-        $.ajax({url: queryURL, method: "GET"})
-       .done(function(response) {
+
+        // CALL THE AJAX
+        $.ajax({
+            url: queryURL, 
+            method: "GET"
+        }).done(function(response){
            var results = response.data;
-           console.log(response.data);
+           console.log(results);
+
+           // CLEAR THE OLD GIFS
+           $("#movies-view").empty();
+   
+           // LOOP OVER THE RESULTS AND APPEND THEM TO THE PAGE
+           for (var i = 0; i < results.length; i++){
+               var gifDiv = $("<div>");         // CREATE A DIV TAG
+               gifDiv.addClass("gifDiv");       // ADD gifDiv CLASS
+               var gifImage = $("<img>");       // CREATE A IMG TAG
+               gifImage.attr("src", results[i].images.fixed_height_small_still.url);            // SRC
+               gifImage.attr("data-still",results[i].images.fixed_height_small_still.url);      // DATA-STILL URL
+               gifImage.attr("data-animate",results[i].images.fixed_height_small.url);          // DATA-ANIMATE URL
+               gifImage.attr("data-state", "still"); // set the image state                     // DATA-STATE ATTRIBUTE
+               gifImage.addClass("image");                                                      // IMAGE CLASS ADDED
+               gifDiv.append(gifImage);                                                         // APPEND THAT TO THE PAGE
+               
+               $("#movies-view").prepend(gifDiv);
+           };
         });
-        // posts gifs to div
-            $("#movies-view").empty(); 
-            var results = response.data; 
-            for (var i = 0; i < results.length; i++){
-                
-                var gifDiv = $("<div>"); 
-                gifDiv.addClass("gifDiv");
-                
-                var gifImage = $("<img>");
-                gifImage.attr("src", results[i].images.fixed_height_small_still.url);
-                gifImage.attr("data-still",results[i].images.fixed_height_small_still.url);
-                gifImage.attr("data-animate",results[i].images.fixed_height_small.url);
-                gifImage.attr("data-state", "still"); // set the image state
-                gifImage.addClass("image");
-                gifDiv.append(gifImage);
-              
-                $("#movies-view").prepend(gifDiv);
-            };
-        }})
+
+    }); // .MOVIE CLICK CLOSER
+
+}); // DOCUMENT.READY CLOSER
